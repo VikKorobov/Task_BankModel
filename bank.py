0cfaaf66be1999115a0740315bbd5a3558da0381
Viktor Korobov <viktor.korobov@korobov-solutions.de>
2026-02-05 14:29:34 +0100

    implement solution for step 2

--- bank.py
+++ bank.py
@@ -6,6 +6,9 @@ class Bank:
 
         self._name: str = name
 
+    def get_name(self) -> str:
+
+        return self._name
 
 class Account(ABC):
 
@@ -15,6 +18,17 @@ class Account(ABC):
         self._holder: Bank = holder
         self._balance: float = 0
 
+    def get_account_number(self) -> str:
+
+        return self._account_number
+    
+    def get_holder(self) -> Bank:
+
+        return self._holder
+    
+    def get_balance(self) -> float:
+
+        return self._balance
 
 class CheckingAccount(Account):
 
@@ -23,6 +37,13 @@ class CheckingAccount(Account):
         super().__init__(account_number, bank)
         self._overdraft_limit: float = overdraft_limit
 
+    def get_overdraft_limit(self) -> float:
+
+        return self._overdraft_limit
+    
+    def set_overdraft_limit(self, limit: float) -> None:
+
+        self._overdraft_limit = limit
 
 class SavingsAccount(Account):
 
@@ -31,6 +52,13 @@ class SavingsAccount(Account):
         super().__init__(account_number, bank)
         self._interest_rate: float = interest_rate
 
+    def get_interest_rate(self) -> float:
+
+        return self._interest_rate
+    
+    def set_interest_rate(self, rate: float) -> None:
+
+        self._interest_rate = rate
 
 class Person:
 
@@ -39,4 +67,20 @@ class Person:
         self._name: str = name
         self._address: str = address
         self._accounts: list[Account] = []
+
+    def get_name(self) -> str:
+
+        return self._name
+    
+    def get_address(self) -> str:
+
+        return self._address
+    
+    def get_accounts(self) -> list[Account]:
+
+        return self._accounts
+    
+    def set_address(self, address: str) -> None:
+
+        self._address = address
 
